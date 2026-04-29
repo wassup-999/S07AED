c78a249fd3cff3fb417666f83ce5c32bbf901758
wassup-999 <sebasquispeperez@gmail.com>
2026-04-29 10:07:28 -0500

    Semana7

--- Assets/Queues/MyQueue.cs
+++ Assets/Queues/MyQueue.cs
@@ -2,7 +2,58 @@ using UnityEngine;
 
 public class MyQueue <T>
 {
+    #region Privates
     private QueueNode<T> head;
     private QueueNode<T> tail;
     private int count;
+    #endregion
+    #region Public Methods
+    public void Enqueue(T value)
+    {
+        QueueNode<T> newNode = new(value);
+        count++;
+        if(head == null && tail == null)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail.SetNext(newNode);
+        tail = newNode;
+    }
+    public T Dequeue() 
+    {
+        if (head == null)
+        {
+            Clear();
+            throw new System.Exception("Queue Empty");
+        }
+            
+        T value = head.Value;   //primero capturar el valor de head del 1ero de la fila
+        head = head.Next;
+
+        count--;
+        return value;
+    }
+    public T Peek()
+    {
+        if (head == null)
+        {           
+            throw new System.Exception("Queue Empty");
+        }
+        T value = head.Value;   
+   
+        return value;
+    }
+
+    public void Clear()
+    {
+        head = null; 
+        tail = null;
+        count = 0;
+    }
+    #endregion
+    #region Getters
+    public int Count => count;
+    #endregion
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,64 @@ using Sirenix.OdinInspector;
 
 public class GameManager : MonoBehaviour
 {
+    public MyQueue<string> BankQueue = new();
+
+
+    public float speed = 0;
+    public float Maxspeed = 100;
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        speed = Mathf.PingPong(Time.time * 0.5f , 5); // para que se mueva de un lado a otro y regrese
+        transform.position = new Vector3(speed , 0 , 0);
+        //speed = Mathf.Lerp(speed, Maxspeed, Time.deltaTime); /// Mover un valor de un minimo y maximo
+        
+    }
+
+    [Button]
+    public void TestMathf()
+    {
+       Debug.Log(Mathf.Clamp(1000, 0, 1));
+       
+       Mathf.Abs(-20); // combierte valores de negativo a positivo
+
+       Mathf.Max(10, 40); // retorna valor maximo
+       Mathf.Min(10, 40); // retorna valor minimo
+        
+       Mathf.Round(5.5f); // redondea el valor
+       Mathf.Floor(4.999f); // retorna el valor más abajo -> 4
+       Mathf.Ceil(6.4f); // retorn valor más alto -> 7
+
+    }
+
+
+
+
+
+    [Button]
+    public void AddToQueue(string name)
+    {
+        BankQueue.Enqueue(name);
+    }
+    [Button]
+    public void DeQueue()
+    {
+        Debug.Log("Pase a ser atendido : " + BankQueue.Dequeue());
+    }
+    [Button]
+    public void Peek()
+    {
+        Debug.Log("Atendiendo turno : " + BankQueue.Peek());
+    }
+    [Button]
+    public void Clear()
+    {
+        BankQueue.Clear();
+    }
+    /*
     public MyStack<string> nameStack = new();
 
      void Start()
@@ -33,5 +91,5 @@ public class GameManager : MonoBehaviour
     [Button]
     public void Count() => Debug.Log(nameStack.Count);
 
-
+    */
 }
